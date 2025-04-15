21e16643798429810f30aa8289e2ee90833d7353
noirandmoca <noirandmoca@gmail.com>
2025-04-15 22:29:40 +0900

    4/15

--- modal.js
+++ modal.js
@@ -1,6 +1,8 @@
 /**
  * モーダルの開閉を制御する
  */
+console.log("あ");/* 確認のための */
+
 document.querySelectorAll('[data-modal-open]').forEach(elem => {
   // 開閉する対象の要素を取得する
   const targetModalId = elem.getAttribute('data-modal-open');
@@ -14,9 +16,9 @@ document.querySelectorAll('[data-modal-open]').forEach(elem => {
     });
   });
 
-
   // 開く
   elem.addEventListener('click', function () {
+    console.log("い");
     modal.classList.add('is-open');
   });
 });
